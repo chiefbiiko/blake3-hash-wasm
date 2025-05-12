186c925eadd7f6f51abf7f4d12c4f6741e406602
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2025-05-12 14:00:13 +0200

    refactor: instantiate pkg sync

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,5 @@
 /* tslint:disable */
 /* eslint-disable */
 
-export function init(): Promise<void>;
 export function hash256hex(msg: Uint8Array): string;
 export function hash(msg: Uint8Array, out: Uint8Array): void;
